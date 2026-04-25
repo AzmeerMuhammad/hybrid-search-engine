eef9e2fed5a481238a748111987470bdeed6bd17
Azmeer!? <164740173+AzmeerMuhammad@users.noreply.github.com>
2026-04-25 18:47:35 +0500

    "week1: add stemmer"

---
+++ src/pipeline/stemmer.py
@@ -0,0 +1,10 @@
+from nltk.stem import PorterStemmer
+ps=PorterStemmer()
+def stem(tokens:list[str])->list[str]:
+    arr=[]
+    for token in tokens:
+        arr.append(ps.stem(token))
+    return arr
+
+tokens=['reserve', 'bank', 'australia', 'rba', 'came', 'january', 'running', 'banks', 'studies']
+print(stem(tokens))
