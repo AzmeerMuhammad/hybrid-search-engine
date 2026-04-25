6d31f1a025b91be5e63085e0f5e96db6145055e7
Azmeer!? <164740173+AzmeerMuhammad@users.noreply.github.com>
2026-04-25 17:30:40 +0500

    "week1: add tokenizer"

---
+++ src/pipeline/tokenizer.py
@@ -0,0 +1,15 @@
+from normalizer import normalize
+
+def tokenize(text: str) -> list[str]:
+    arr=[]
+    for token in text.split():
+        if len(token)>=2:
+            arr.append(token)
+    return arr
+
+
+raw = "The Reserve Bank of Australia (RBA) came into being on 14 January 1960"
+normalized=normalize(raw)
+tokens=tokenize(normalized)
+print(normalized)
+print(tokens)
