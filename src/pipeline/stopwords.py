72197a1a1c295f0e548e3049edbfbe45f4bcda23
Azmeer!? <164740173+AzmeerMuhammad@users.noreply.github.com>
2026-04-25 17:54:19 +0500

    "week1: add stopwords filter"

---
+++ src/pipeline/stopwords.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk.corpus import stopwords
+sw = set(stopwords.words("english"))
+def remove_stopwords(tokens:list[str])->list[str]:
+    arr=[]
+    for token in tokens:
+        if token not in sw:
+            arr.append(token)
+    return arr
+
+tokens = ['the', 'reserve', 'bank', 'of', 'australia', 'rba', 'came', 'into', 'being', 'on', 'january']
+print(remove_stopwords(tokens))
+
+#using list comp
+# return [token for token in tokens if token not in sw]
+
